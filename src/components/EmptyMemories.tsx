d3758c5d5ecdd109dda9355654b2c851e3215a2f
WildBarreto <wildsbsrreto@gmail.com>
2023-05-20 17:30:33 -0300

    Criação de memorias

--- src/components/EmptyMemories.tsx
+++ src/components/EmptyMemories.tsx
@@ -1,12 +1,13 @@
 export default function EmptyMemories() {
   return (
-    <div className="flex flex-1 items-center justify-center">
-      <p className="w-[340px] text-center leading-relaxed">
-        Você não registrou nenhuma lembrança, comece a{" "}
+    <div className="flex flex-1 items-center justify-center p-16">
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, começa{' '}
         <a href="" className="underline hover:text-gray-50">
-          criar agora!
+          criar agora
         </a>
+        !
       </p>
     </div>
-  );
+  )
 }
